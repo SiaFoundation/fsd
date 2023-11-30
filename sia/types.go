0e289691fd606d69d3f1756f9cc57081437bbefd
Nate Maninger <me@n8m.us>
2023-11-29 22:09:12 -0800

    sia: add json tags

--- sia/types.go
+++ sia/types.go
@@ -7,25 +7,27 @@ import (
 type (
 	// A RenterdData links IPFS block data to an object stored on a renterd node
 	RenterdData struct {
-		Key       string
-		Offset    uint64
-		FileSize  uint64
-		BlockSize uint64
+		Bucket    string `json:"bucket"`
+		Key       string `json:"key"`
+		Offset    uint64 `json:"offset"`
+		FileSize  uint64 `json:"filesize"`
+		BlockSize uint64 `json:"blocksize"`
 	}
 
 	// RenterdMeta links IPFS block metadata to an object stored on a renterd
 	// node
 	RenterdMeta struct {
-		Key    string
-		Offset uint64
-		Length uint64
+		Bucket string `json:"bucket"`
+		Key    string `json:"key"`
+		Offset uint64 `json:"offset"`
+		Length uint64 `json:"length"`
 	}
 
 	// A Link is a link to another IPFS node
 	Link struct {
-		CID  cid.Cid
-		Name string
-		Size uint64
+		CID  cid.Cid `json:"cid"`
+		Name string  `json:"name"`
+		Size uint64  `json:"size"`
 	}
 
 	// A Block is an IPFS chunk with metadata for efficient storage and
