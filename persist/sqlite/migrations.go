3f2825fe1031c30f22bc9c86392572592cbdaade
Nate Maninger <me@n8m.us>
2024-05-05 15:06:24 -0700

    sqlite: fix migration

--- persist/sqlite/migrations.go
+++ persist/sqlite/migrations.go
@@ -8,7 +8,7 @@ func migrateV2(tx *txn, _ *zap.Logger) error {
 	const query = `ALTER TABLE pinned_blocks ADD COLUMN created_at INTEGER NOT NULL DEFAULT 0;
 ALTER TABLE pinned_blocks ADD COLUMN updated_at INTEGER NOT NULL DEFAULT 0;
 ALTER TABLE pinned_blocks ADD COLUMN last_announcement INTEGER NOT NULL DEFAULT 0;
-CREATE INDEX pinned_blocks_block_id_last_announcement ON (block_id, last_announcement ASC);`
+CREATE INDEX pinned_blocks_block_id_last_announcement ON pinned_blocks (block_id, last_announcement ASC);`
 
 	_, err := tx.Exec(query)
 	return err
